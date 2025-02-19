4d38a31b225d826a9fcae00710df62516a67d974
Fariel Dzaky <169522892+LechyRevolutionn@users.noreply.github.com>
2025-02-19 12:28:21 +0700

    Menbuat Project Website Portofolio

---
+++ PORTOFOLIO/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').config} */
+modules.exports = {
+    content: [
+        './public/index.html',
+    ],
+    theme: {
+      extend: {
+        letterSpacing: {
+          ngempit: '-0.5em'
+          ngangkang: '.5em',
+        }
+      },
+    },
+    plugins: [],
+}
+
+/** h */
